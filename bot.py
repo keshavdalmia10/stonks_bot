c05d2c7f42ea608ceafb1be89a6f1092f85f80df
Keshav Dalmia <keshavdalmia10@gmail.com>
2021-11-25 03:20:46 +0530

    added chart

--- bot.py
+++ bot.py
@@ -4,6 +4,11 @@ import random
 import yfinance as yf
 from discord.ext import commands
 from dotenv import load_dotenv
+import matplotlib.pyplot as plt
+
+import discord
+
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -79,10 +84,50 @@ async def get_list(ctx):
    
  
     for key, value in stocks.items():
-        print(key, ' : ', value)
-        
+        # await ctx.send(key)
+        # await ctx.send(value)
 
+        await ctx.send(' {} : {}'.format(key, value))
+
+@bot.command(name="prev-stock-data", help="Check previous day stock data of a company.")
+async def stock_data(ctx, stock_company):
+
+    if stock_company in stocks.keys:
+        stock_company_df = yf.download(stock_company, period="2d")
+        msg = create_msg(stock_company, stock_company_df)
+
+        stock_company_df = yf.download(
+            stock_company, period="2d", interval="1m")
+        stock_company_df[0:390].plot(y='Close', linewidth=0.85)
+
+        plt.xlabel('Datetime')
+        plt.ylabel('Close')
+        plt.title('Stock prices of {company} for previous day'.format(
+            company=stock_company))
+
+        plt.savefig('images/stock_previous_day.png')
+
+        await ctx.send(msg, file=discord.File('images/stock_previous_day.png'))
+
+        os.remove('images/stock_previous_day.png')
+    else:
+        await ctx.send("Stock data for {stockCompany} doesn't exist!".format(stockCompany=stock_company))  
+
+
+def create_msg(stocks, stocks_df):
+    date = str(stocks_df.head(1).index[0]).split(' ')[0]
+    msg = '''\
+        {company} EOF Data
+        - Date: {Date}
+        - Open: {Open}
+        - High: {High}
+        - Low: {Low}
+        - Close: {Close}
+        - Adj Close: {Adj_Close}
+        - Volume: {Volume}\
+     '''.format(company=stocks, Date=date, Open=stocks_df.iat[0, 0], High=stocks_df.iat[0, 1], Low=stocks_df.iat[0, 2], Close=stocks_df.iat[0, 3], Adj_Close=stocks_df.iat[0, 4], Volume=stocks_df.iat[0, 5])
+
+    return msg      
 
 bot.run(TOKEN)
-
 
